869fca1ebee0df09bdb4edf3a0bcca067e7a0553
Anwesh43 <anweshthecool0@gmail.com>
2018-12-31 11:06:28 +0530

    adding scale functions

--- LineCombStepStage.ts
+++ LineCombStepStage.ts
@@ -8,6 +8,25 @@ const sizeFactor : number = 2.8
 const color : string = "#43A047"
 const backColor : string = "#bdbdbd"
 
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1/n, maxScale(scale, i, n)) * n
+}
+
+const scaleFactor : Function = (scale : number) => Math.floor(scale / scDiv)
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
+
 class LineCombStepStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
