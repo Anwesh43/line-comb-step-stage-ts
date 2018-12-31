c5facbe81de63e74c2a201440d9bcdd24ccc703a
Anwesh43 <anweshthecool0@gmail.com>
2018-12-31 11:25:55 +0530

    using renderer in LineCombStepStage

--- LineCombStepStage.ts
+++ LineCombStepStage.ts
@@ -60,6 +60,7 @@ const drawLCSNode : Function = (context : CanvasRenderingContext2D, i : number, 
 class LineCombStepStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -71,11 +72,14 @@ class LineCombStepStage {
     render() {
         this.context.fillStyle = backColor
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
